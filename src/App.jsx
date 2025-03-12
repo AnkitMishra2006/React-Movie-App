6a9ac2a3622dc8eb7a5773bf9c734d91556f1587
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-03-12 18:29:16 +0530

    all done, extra features remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Search from "./components/Search";
 import { useState, useEffect } from "react";
+import { useDebounce } from "react-use";
+import Spinner from "./components/spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -17,10 +20,18 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
+    setIsLoading(true);
+    setErrorMessage("");
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -29,18 +40,24 @@ const App = () => {
 
       const data = await response.json();
 
-      if (data.Response === 'False') {
+      if (data.Response === "False") {
         setErrorMessage(data.Error || "Failed to Fetch Movies");
+        setMovieList([]);
+        return;
       }
+      setMovieList(data.results || []);
     } catch (error) {
       console.error(`Error Fetching Movies: ${error}`);
       setErrorMessage("Error Fetching Movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
   return (
     <main>
       <div className="pattern"></div>
@@ -58,8 +75,18 @@ const App = () => {
           ></Search>
         </header>
         <section className="all-movies">
-          <h2>All Movies</h2>
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          <h2 className="mt-[20px] ">All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie}></MovieCard>
+              ))}
+            </ul>
+          )}
         </section>
       </div>
     </main>
